abfd63bef5990e93ad967a428313fa673484c5ff
Iv Esersky <ivanaesersky@hotmail.com>
2022-01-16 23:08:45 -0300

    Arreglo de estilo en product detail

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,17 +1,18 @@
 import { styles } from "./styles";
 import React from "react";
+import "../components/containers/itemListContainer.css";
 
 export const ItemDetail = ({ item }) => {
     return (
         <>
             {item &&
                 item.map((item) => (
-                    <div key={item.id}>
+                    <div key={item.id} className="container">
                         <img
                             src={item.pictureUrl}
                             alt={`${item.id}-${item.title}`}
                         />
-                        <section style={styles.flexCol}>
+                        <section style={styles.flexCol} className="blurbg">
                             <h1>{item.title}</h1>
                             <p>{item.description}</p>
                             <h2>${item.price}</h2>
