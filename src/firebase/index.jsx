101a6c4dcdd38029199e4e20f1c6c60ef57154eb
Ivy <ivanaesersky@hotmail.com>
2022-01-28 13:04:55 -0300

    Instalacion Firebase

---
+++ src/firebase/index.jsx
@@ -0,0 +1,11 @@
+import firebase from "./firebase/app";
+import "@firebase/firestore";
+
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyBzDQ9VfPCXTv2qGhnRi_tt3Gzn7XtBdJM",
+    authDomain: "coder-ecommerceiv.firebaseapp.com",
+    projectId: "coder-ecommerceiv",
+    storageBucket: "coder-ecommerceiv.appspot.com",
+    messagingSenderId: "1030758641377",
+    appId: "1:1030758641377:web:36f3cd331fd9b78aa691a0",
+});
